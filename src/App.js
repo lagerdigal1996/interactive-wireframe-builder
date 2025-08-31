e16980839bb1f3011b233c887ebcaa689d6f8e95
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-08-31 04:01:57 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Interactive Wireframe Builder</h1>
+      <p>Start building your interactive wireframes!</p>
+      {/* Add your drag-and-drop wireframe components here */}
+    </div>
+  );
+};
+
+export default App;
